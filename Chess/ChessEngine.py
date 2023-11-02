d432036e3cd8b28515ed2d03124d9487c46ab868
Rusran <ralim002@gmail.com>
2023-11-02 10:52:53 -0400

    Undo movment implementation

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -1,51 +1,31 @@
-"""
-This class will hold the data for the current state of the game
-Also will determin valid moves and move history
-"""
+'''
+Storing all the information about the current state of chess game.
+Determining valid moves at current state.
+It will keep move log.
+'''
 
-##using 2d list to make the board. we should change this using numpy for better efficiency 
 class GameState():
     def __init__(self):
-        #8x8 2d list, first character is the color the second char is the peice 
+        '''
+        Board is an 8x8 2d list, each element in list has 2 characters.
+        The first character represtents the color of the piece: 'b' or 'w'.
+        The second character represtents the type of the piece: 'R', 'N', 'B', 'Q', 'K' or 'p'.
+        "--" represents an empty space with no piece.
+        '''
         self.board = [
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
-            ["--", "--", "--", "--", "--", "--" , "--", "--"],
-            ["--", "--", "--", "--", "--", "--" , "--", "--"],
-            ["--", "--", "--", "--", "--", "--" , "--", "--"],
-            ["--", "--", "--", "--", "--", "--" , "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
-            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
-        ]
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+        
+        
+        self.white_to_move = True
+        self.move_log = []
         
 
-        self.white_To_Move = True
-        self.move_Log = []   
+    
         
-    def makeMove(self, move):
-        self.board[move.startRow][move.startCol] = "--"
-        self.board[move.endCol][move.endCol] = move.pieceMove
-        self.move_Log.append(move)
-        self.white_To_Move = not self.white_To_Move
-        
-class Move:
-
-    ranks_to_rows = {"1": 7, "2": 6, "3": 5, "4": 4,
-                     "5": 3, "6": 2, "7": 1, "8": 0}
-    rows_to_ranks = {v: k for k, v in ranks_to_rows.items()}
-    files_to_cols = {"a": 0, "b": 1, "c": 2, "d": 3,
-                     "e": 4, "f": 5, "g": 6, "h": 7}
-    cols_to_files = {v: k for k, v in files_to_cols.items()}
-
-    def __init__(self, startSQUARE, endSQUARE, board): 
-        self.startRow = startSQUARE[0]
-        self.startCol = startSQUARE[1]
-        self.endRow = endSQUARE[0]
-        self.endCol = endSQUARE[1]
-        self.pieceMove = board[self.startRow][self.startCol]
-        self.pieceCaptured = board[self.endRow][self.endCol]
-        
-    def getChessNotation(self):
-        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
-    def getRankFile(self, r, c):
-        return self.cols_to_files[c] + self.rows_to_ranks[r]       
--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -43,22 +43,6 @@ def main():
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
-            elif e.type == p.MOUSEBUTTONDOWN:
-                location = p.mouse.get_pos()
-                col = location[0]//SQUARE
-                row = location[1]//SQUARE
-                if SQUARESelected == (row, col):
-                    SQUARESelected = ()
-                    playerClicks = []
-                else:  
-                     SQUARESelected = (row,col)
-                     playerClicks.append(SQUARESelected)
-                if len(playerClicks) == 2:
-                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
-                    print(move.getChessNotation())
-                    gs.makeMove(move)
-                    SQUARESelected = ()
-                    playerClicks = []
                     
         draw_Game_State(screen, gs)
         clock.tick(MAX_FPS)
