eb25405f8374d2e085582f952144d80c5016ffb0
Rusran <ralim002@gmail.com>
2023-11-09 17:48:08 -0500

    Revert "added numpy but still working on it"
    This reverts commit cd24ef27953b996acc02cc3d58d0b130382bfe10.

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -1,6 +1,3 @@
-import numpy as np
-
-
 '''
 Storing all the information about the current state of chess game.
 Determining valid moves at current state.
@@ -15,8 +12,8 @@ class GameState():
         The second character represtents the type of the piece: 'R', 'N', 'B', 'Q', 'K' or 'p'.
         "--" represents an empty space with no piece.
         '''
-        self.board = (
-            [["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
@@ -24,11 +21,10 @@ class GameState():
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
-            )
         
         
         self.whiteToMove = True
-        self.moveLog = ([])
+        self.moveLog = []
         
         
     def makeMove(self, move):
